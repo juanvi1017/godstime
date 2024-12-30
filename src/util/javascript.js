9ddd5d71006ef8fbcfd6535d46ef5221bd88e075
jcaceres <jvcaceres17@gmail.com>
2024-12-30 12:04:50 -0500

    Fix: whatsapp

--- src/util/javascript.js
+++ src/util/javascript.js
@@ -1,11 +1,11 @@
 
 export const chatWhatsApp = (product) => {
-  const win = window.open(`https://wa.me/573007260320?text=Hola!%20estoy%20interesado%20en%20tu%20producto%20${product}`, '_blank');
+  const win = window.open(`https://wa.me/573023634037?text=Hola!%20estoy%20interesado%20en%20tu%20producto%20${product}`, '_blank');
   win.focus();
 }
 
 export const chatWhatsAppHelp = () => {
-  const win = window.open(`https://wa.me/573007260320?text=Hola!%20m%20interesado%20en%20tu%20producto%20necesito%20ayuda`, '_blank');
+  const win = window.open(`https://wa.me/573023634037?text=Hola!%20m%20interesado%20en%20tu%20producto%20necesito%20ayuda`, '_blank');
   win.focus();
 }
 
